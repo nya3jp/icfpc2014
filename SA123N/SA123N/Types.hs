630554224c8464c19644b78e670a20486d4e2d6d
nushio <nushio@nuwheezy.paraiso-lang.org>
2014-07-26 10:57:27 +0900

    new  bi-2

--- SA123N/SA123N/Types.hs
+++ SA123N/SA123N/Types.hs
@@ -1,9 +1,12 @@
+{-# LANGUAGE TemplateHaskell #-}
 module SA123N.Types where
 
-data Breeder m a =
+import Control.Lens
+
+data Breeder m a = Breeder
   { _mutate :: Double -> a -> m a
   , _crossover :: Double -> a -> a -> m a
   , _triangulate :: a -> a -> a -> m a
   }
   
-  
+makeLenses ''Breeder  
