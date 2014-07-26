08a2f63281d9bdaa6102bf4bf2872657643d15fd
Hideyuki Tanaka <tanaka.hideyuki@gmail.com>
2014-07-26 19:10:48 +0900

    dsl

--- LambdaManP/main-jm-nors.hs
+++ LambdaManP/main-jm-nors.hs
@@ -8,7 +8,7 @@ import System.Environment
 import Unsafe.Coerce
 
 import Desugar
-import DSL07261800
+import DSL
 
 -----
 type X = Int
@@ -21,25 +21,25 @@ type Pos = (Int, Int)
 progn :: LMan ()
 progn = do
   nth :: forall a. Expr ([a] -> Int -> a) <- nth'
-  
 
-  (tileValue :: Expr (Int -> Int)) <- fun1 $ \i ->                                              
+
+  (tileValue :: Expr (Int -> Int)) <- fun1 $ \i ->
     ite (i.==0) 0 $ ite (i.==2) 1000 $ ite (i.==3) 10000 $ 1
 
   let  mapAt :: Expr [[Int]] -> Expr Int -> Expr Int -> Expr Int
        mapAt chizu ix iy = (call2 nth (call2 nth chizu iy) ix)
-  let mkDirValuePill (dx,dy) = do 
+  let mkDirValuePill (dx,dy) = do
         rec
          (dirValueRet :: Expr ([[Int]] -> Int -> Int -> Int))
-           <- fun3 $ \chizu manX manY -> 
+           <- fun3 $ \chizu manX manY ->
                      let info = (mapAt chizu manX manY) in
-                     ite (info .== 0) 0 $ 
+                     ite (info .== 0) 0 $
                      (call1 tileValue info) + (call3 dirValueRet chizu (manX+dx)  (manY+dy))`div`2
         return dirValueRet
 
 
-  mkDirValueTotal (dx,dy) = 
-    
+  mkDirValueTotal (dx,dy) =
+
 
   dirValueN  <- mkDirValueTotal (0,-1)
   dirValueE  <- mkDirValueTotal (1,0)
@@ -47,18 +47,18 @@ progn = do
   dirValueW  <- mkDirValueTotal (-1,0)
 
   (step :: Expr (AIState -> World -> (AIState,Int))) <- fun2 $ \aist world ->
-    let manPos :: Expr Pos 
+    let manPos :: Expr Pos
         manPos = car $ cdr $ car $ cdr world
         manX :: Expr Int
         manX = car manPos
         manY :: Expr Int
         manY = cdr manPos
-        
+
         chizu :: Expr [[Int]]
         chizu = car world
 
 
-        
+
         scoreN, scoreE, scoreS, scoreW :: Expr Int
         scoreN = call3 dirValueN chizu manX manY
         scoreE = call3 dirValueE chizu manX manY
@@ -72,7 +72,7 @@ progn = do
              (3 :: Expr Int)
 
     in
-     
+
     dbugn (call3 dirValueN chizu manX manY) `Seq`
     cons aist d2
   expr $ cons (0 :: Expr AIState) step
