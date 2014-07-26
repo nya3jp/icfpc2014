0139e656c6adc22a847fc917d4f5d9b430130978
MAYAH <mayah@mayah.jp>
2014-07-26 12:51:18 +0900

    lambdaman can add value.

--- simulator/lambdaman.ml
+++ simulator/lambdaman.ml
@@ -1,5 +1,8 @@
-type linstruction =
-  | LLdc of int
+type value =
+  | VInt of int32
+
+type instruction =
+  | LLdc of int32
   | LLd  of int * int
   | LAdd
   | LSub
@@ -24,24 +27,52 @@ type linstruction =
   | LTrap
   | LDbug
 
-(*
-type env = {
-  c: int32; (* control register *)
-  s: int32; (* data stack register *)
-  d: int32; (* control stack register *)
-  e: int32; (* environment frame register *)
-
-  data_stack: data_stack;
-  control_stack: control_stack;
-  environment_frame_chain: environment_frame_chain;
+type environment = {
+  parent : environment option;
 }
-*)
 
-(*
-%c: control register (program counter / instruction pointer)
-%s: data stack register
-%d: control stack register
-%e: environment frame register
-*)
+type machine = {
+  mutable pc: int;
+  mutable data: value Stack.t;
+  mutable control: int Stack.t;
+  (* mutable env: *)
+}
 
-(*let rec eval env = function *)
+type program = instruction array
+
+let make_initial_machine () = {
+  pc = 0;
+  data = Stack.create ();
+  control = Stack.create ();
+}
+
+let print_value = function
+  | VInt x ->
+     print_endline (Int32.to_string x)
+;;
+
+let eval machine = function
+  | LLdc n ->
+     Stack.push (VInt n) machine.data;
+     machine.pc <- machine.pc
+  | LAdd ->
+     let y = Stack.pop machine.data in
+     let x = Stack.pop machine.data in
+     let (VInt xx) = x
+     and (VInt yy) = y in
+     let z = Int32.add xx yy in
+     Stack.push (VInt z) machine.data;
+     machine.pc <- machine.pc + 1
+  | LDbug ->
+     let x = Stack.pop machine.data in
+     print_value x;
+     machine.pc <- machine.pc + 1
+
+(* *)
+let _ =
+  let machine = make_initial_machine () in
+  eval machine (LLdc (Int32.of_int 1));
+  eval machine (LLdc (Int32.of_int 2));
+  eval machine LAdd;
+  eval machine LDbug;
+;;
