903bcbdefd64382a0f0ca2a29f41ee659789dca3
nushio <nushio@nuwheezy.paraiso-lang.org>
2014-07-26 15:57:41 +0900

    get food

--- LambdaManP/main-jm.hs
+++ LambdaManP/main-jm.hs
@@ -1,17 +1,18 @@
-{-# LANGUAGE FlexibleInstances, GADTs, RecursiveDo, ScopedTypeVariables #-}
+{-# LANGUAGE FlexibleInstances, GADTs, RecursiveDo, ScopedTypeVariables, RecordWildCards, RankNTypes, ImpredicativeTypes #-}
 
 import qualified Data.Map as M
 import Data.Maybe
 import Data.Char
 import Debug.Trace
 import System.Environment
+import Unsafe.Coerce
 
 import Desugar
 import DSL
 
 -----
 type X = Int
-type World = (X, (ManState, (X, X)))
+type World = ([[Int]], (ManState, (X, X)))
 type ManState = (X, (Pos, X))
 type AIState = X
 type Pos = (Int, Int)
@@ -19,6 +20,9 @@ type Pos = (Int, Int)
 
 progn :: LMan ()
 progn = do
+  nth :: forall a. Expr ([a] -> Int -> a) <- nth'
+  
+  
   (step :: Expr (AIState -> World -> (AIState,Int))) <- fun2 $ \aist world ->
     let manPos :: Expr Pos 
         manPos = car $ cdr $ car $ cdr world
@@ -27,7 +31,14 @@ progn = do
         manY :: Expr Int
         manY = cdr manPos
         d = ite (manY .<= 1) 3 $ ite (manX .< 17) 1 0 
+        
+        chizu :: Expr [[Int]]
+        chizu = car world
+        
+        
     in
+    dbugn manY `Seq` 
+    dbugn (call2 nth (call2 nth chizu 16) 16) `Seq`
     cons aist d
   expr $ cons (0 :: Expr AIState) step
 
