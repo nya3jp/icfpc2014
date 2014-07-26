3b9bdaf079fe0462f02a61a9e1be9fb821329f8a
Ubuntu <ubuntu@ip-172-31-47-7.us-west-2.compute.internal>
2014-07-26 23:59:50 +0000

    add an setup script

---
+++ setup-ec2.sh
@@ -0,0 +1,3 @@
+sudo apt-get -y update
+sudo apt-get -y install git ghc emacs screen cabal-install
+wget http://www.haskell.org/ghc/dist/7.8.3/ghc-7.8.3-x86_64-unknown-linux-deb7.tar.bz2 -O ghc.tar.bz2
