dfb5239a2908920f15947e3b29b3b2e97029f6cb
MAYAH <mayah@mayah.jp>
2014-07-26 19:40:02 +0900

    hoge

--- simulator/simulator.ml
+++ simulator/simulator.ml
@@ -1,18 +1,54 @@
 (* simulator code will be here. *)
 
 (* simulator state *)
-(*let t = {
+type t = {
   lambdamans: Lambdaman.t array;
-  ghosts:    Ghost.t array;
-  field:     World.t;
+  ghosts:     Ghost.t array;
+  map:        World.map;
+  mutable fruitExists: bool;
 }
 
-let make lambdamans ghosts world = {
-  lambdamans = lambdamans;
-  ghosts     = ghosts;
-  field      = field;
-}
+let make map lambdaman_programs ghost_programs =
+  if Array.length lambdaman_programs <= 0 || 2 < Array.length lambdaman_programs then
+    failwith "# of lambdaman program is wrong";
 
-let tick =
-  failwith "Not implemented yet"
+  if Array.length ghost_programs <= 0 || 4 < Array.length ghost_programs then
+    failwith "# of ghost program is wrong";
+
+  let lambdaman_index = ref 0
+  and ghost_index = ref 0 in
+
+  let lambdamans = ref []
+  and ghosts = ref [] in
+
+  Array.iteri ( fun y line ->
+    Array.iteri (fun x cell ->
+      match cell with
+      | World.CLambdaManStart ->
+         let t = Lambdaman.make !lambdaman_index x y lambdaman_programs.(!lambdaman_index) in
+         lambdamans := t :: !lambdamans;
+         incr lambdaman_index
+      | World.CGhostStart ->
+         let t = Ghost.make !ghost_index x y ghost_programs.(!ghost_index mod (Array.length ghost_programs)) in
+         ghosts := t :: !ghosts;
+         incr ghost_index
+    ) line
+  ) map;
+
+  {
+    lambdamans = Array.of_list (List.rev !lambdamans);
+    ghosts = Array.of_list (List.rev !ghosts);
+    map = map;
+    fruitExists = false;
+  }
+;;
+
+(*
+let tick tick_id t =
+  move_lambdamans_if_necessary tick_id t;
+  move_ghosts_if_necessary tick_id t;
+  check_fright_mode_deactivating tick_id t;
+  check_fruit_appearing tick_id t;
 *)
+
+
