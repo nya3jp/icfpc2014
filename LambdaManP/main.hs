b7afd7a6e87c16abf1458a0e662d962a0ec60bb7
Hideyuki Tanaka <tanaka.hideyuki@gmail.com>
2014-07-27 15:16:23 +0900

    hoge

--- LambdaManP/main.hs
+++ LambdaManP/main.hs
@@ -29,12 +29,12 @@ type WorldState = (Map, (Int, (Int, Int)))
 vadd :: Expr (Int, Int) -> Expr (Int, Int) -> Expr (Int, Int)
 vadd a b = cons (car a + car b) (cdr a + cdr b)
 
-(bfs :: Expr [[Int]] -> Expr (Queue (Int, Int)) -> Expr (Int, Int) -> Expr (Int, Int) -> Expr (Int, Int), bfsDef) = def4 "paint" $ \bd q initPos out -> comp $ do
-  q ~= enqueue initPos emptyQueue
+bfs :: Expr (Mat Int) -> Expr (Int, Int) -> Expr (Int, Int)
+(bfs, bfsDef) = def2 "paint" $ \bd initPos -> with (enqueue initPos emptyQueue) $ \q -> with (cons 0 0) $ \out -> comp $ do
 
   while (lnot $ isEmptyQueue q) $ comp $ do
     let pos = car $ dequeue q
-    let cell = getMat (car pos) (cdr pos) bd
+    let cell = peekMat (car pos) (cdr pos) bd
 
     debug $ cons (i 10002) (cons pos cell)
 
@@ -47,7 +47,7 @@ vadd a b = cons (car a + car b) (cdr a + cdr b)
           )
           $ comp $ do
               e $ ite ((cell .== 0) ||| (cell .== 6)) (0 :: Expr Int) $ comp $ do
-                bd ~= setMat (car pos) (cdr pos) 0 bd
+                bd ~= pokeMat (car pos) (cdr pos) 0 bd
                 -- record path
                 q ~= enqueue (vadd pos (cons 0    1   )) q
                 q ~= enqueue (vadd pos (cons 0    (-1))) q
@@ -67,17 +67,13 @@ type AIState = X
 type Pos = (Int, Int)
 
 (step, stepDef) = def2 "step" $ \st world -> comp $ do
-  let bd = car world
+  let bd = toMat $ car world
       -- pos = cadr $ cadr world
       pos = cons 11 12
 
-  debug pos
-  debugn 123
-  debug $ (cons (i 777) $ bfs bd emptyQueue pos (cons 0 0))
+  debug bd
+  debug $ cons (i 777) $ bfs bd pos
 
-  return ()
-
-{-
 progn :: LMan ()
 progn = do
   libDef
@@ -85,57 +81,55 @@ progn = do
   bfsDef
   stepDef
 
-  expr $ cons (0 :: Expr Int) (Closure "step")
--}
+  rtn $ cons (0 :: Expr Int) (Closure "step")
 
+{-
 progn :: LMan ()
 progn = do
   libDef
 
-  cexpr $ do
-    cwith (mkArray (list $ map fromIntegral [0..9 :: Int] :: Expr [Int])) $ \arr -> do
-      debug arr
+  -- cexpr $ do
+  --   cwith (mkArray (list $ map fromIntegral [0..9 :: Int] :: Expr [Int])) $ \arr -> do
+  --     debug arr
 
-      for 0 10 $ \i -> do
-        debug $ cons i $ peek i arr
+  --     for 0 10 $ \i -> do
+  --       debug $ cons i $ peek i arr
 
-      for 0 10 $ \i -> do
-        arr ~= poke i (peek i arr * peek i arr) arr
+  --     for 0 10 $ \i -> do
+  --       arr ~= poke i (peek i arr * peek i arr) arr
 
-      debug arr
+  --     debug arr
 
-      for 0 10 $ \i -> do
-        debug $ cons i $ peek i arr
+  --     for 0 10 $ \i -> do
+  --       debug $ cons i $ peek i arr
 
-{-
+  -- cexpr $ do
+  --   debug $ lreverse $ list [1, 2, 3, 4, 5 :: Expr Int]
+  --   debugn 1234
 
-  cexpr $ do
-    debug $ lreverse $ list [1, 2, 3, 4, 5 :: Expr Int]
-    debugn 1234
+  --   cwith 123 $ \i -> do
+  --     debugn i
+  --     debugn $ i * 2
+  --     i ~= i * i
+  --     debugn i
 
-    cwith 123 $ \i -> do
-      debugn i
-      debugn $ i * 2
-      i ~= i * i
-      debugn i
+  --   cwith 0 $ \i -> do
+  --     while (i .< 10) $ comp $ do
+  --       debugn i
+  --       i ~= i + 1
 
-    cwith 0 $ \i -> do
-      while (i .< 10) $ comp $ do
-        debugn i
-        i ~= i + 1
-
-  expr $ do
-    with (enqueue 1 emptyQueue :: Expr (Queue Int)) $ \q1 ->
-      with (enqueue 2 q1) $ \q2 ->
-      with (enqueue 3 q2) $ \q3 ->
-      with (dequeue q3) $ \r4 ->
-      with (dequeue $ cdr r4) $ \r5 ->
-      with (dequeue $ cdr r5) $ \r6 -> comp $ do
-        debug q2
-        debug q3
-        debug r4
-        debug r5
-        debug r6
+  -- expr $ do
+  --   with (enqueue 1 emptyQueue :: Expr (Queue Int)) $ \q1 ->
+  --     with (enqueue 2 q1) $ \q2 ->
+  --     with (enqueue 3 q2) $ \q3 ->
+  --     with (dequeue q3) $ \r4 ->
+  --     with (dequeue $ cdr r4) $ \r5 ->
+  --     with (dequeue $ cdr r5) $ \r6 -> comp $ do
+  --       debug q2
+  --       debug q3
+  --       debug r4
+  --       debug r5
+  --       debug r6
 -}
 
 main :: IO ()
