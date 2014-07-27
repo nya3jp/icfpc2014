e63c346006c7585d60290b4093ca41bc992fa637
MAYAH <mayah@mayah.jp>
2014-07-27 18:12:32 +0900

    Allow multiple ghosts.

--- simulator/main.ml
+++ simulator/main.ml
@@ -1,3 +1,8 @@
+let parse_ghosts ghosts =
+  let list = Str.split (Str.regexp ",") ghosts in
+  Array.of_list list
+;;
+
 (* *)
 let main () =
   Printexc.record_backtrace true;
@@ -6,15 +11,19 @@ let main () =
 
   let fieldName = Sys.argv.(1) in
   let lambdamanFilename = Sys.argv.(2) in
-  let ghostFilename = Sys.argv.(3) in
+  let ghostFilenames = Sys.argv.(3) in
 
   let field = FieldReader.read fieldName in
   let ai = LambdamanReader.read lambdamanFilename in
-  let ghost = GhostAiReader.read ghostFilename in
+  let ghosts = Array.map (fun ghostFilename ->
+    GhostAiReader.read ghostFilename
+  ) (parse_ghosts ghostFilenames) in
+
+
 
   (* print_string (Field.string_of_field field); *)
 
-  let simulator = Simulator.make field [|ai|] [|ghost|] in
+  let simulator = Simulator.make field [|ai|] ghosts in
   Simulator.run simulator
 ;;
 
