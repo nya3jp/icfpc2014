61ba29e041fb83ae7ba656824b2b48027ed10625
nushio <nushio@nuwheezy.paraiso-lang.org>
2014-07-27 19:52:17 +0900

    cornercase generator.hs

---
+++ LambdaManP/cornercase.hs
@@ -0,0 +1,57 @@
+{-# LANGUAGE FlexibleInstances, GADTs, RecursiveDo, ScopedTypeVariables, RecordWildCards, RankNTypes, ImpredicativeTypes #-}
+
+import qualified Data.Map as M
+import Data.Maybe
+import Data.Char
+import Data.Function (on)
+import Data.List (intercalate, sortBy)
+import Debug.Trace
+import Safe
+import System.Environment
+import System.IO
+import System.IO.Unsafe
+import System.FilePath
+import System.Random
+import System.Process
+import Unsafe.Coerce
+import Text.Printf
+import Desugar
+import DSL
+import Lib
+import System.Process
+
+
+-----
+type X = Int
+type Direction = Int
+type Clock = Int
+type World = ([[Int]], (ManState, ([GhostState], FruitState)))
+--               vit                    lives score
+type ManState = (Int, (Pos, (Direction, (Int, Int  ))))
+type GhostState = (Int, (Pos , Direction))
+type FruitState = Int
+type AIState = ((Mat Int),(Clock,X))
+type Pos = (Int, Int)
+
+
+(step :: Expr AIState -> Expr World0 -> Expr (AIState, Int), stepDef) =
+  def2 "step" $ \aist world0 -> 
+    let
+        gss :: Expr [GhostState]
+        gss = car $ cdr $ cdr world
+        gs1 :: Expr GhostState
+        gs1 = car $ gs1
+        gY :: Expr Int
+        gY = cdr $ cdr gs1
+        
+        manState :: Expr ManState
+        manState = car $ cdr world
+        
+        d = ite (gY .== 1) 2 1
+    in 
+        rtn (aist, d)
+      
+  
+main :: IO ()
+main = do
+  writeFile "../LambdaMan/cornercase-think.gcc" $ compile progn    
