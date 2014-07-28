a69c1ee829a3bc84e52195659a568125a5573b6f
nushio <nushio@nuwheezy.paraiso-lang.org>
2014-07-28 19:48:12 +0900

    update script

--- solution/update.sh
+++ solution/update.sh
@@ -6,6 +6,4 @@ set -ex
 
 rm -rf .build
 mkdir -p .build
-cp LambdaManP/madoka.asm .build/madoka.asm
-python alice/alice.py -r alice/karen.py > .build/karen.asm
-python alice/shino.py .build/madoka.asm .build/karen.asm > solution/lambdaman.gcc
+cp ./LambdaManP/saikyo.gcc solution/lambdaman.gcc
