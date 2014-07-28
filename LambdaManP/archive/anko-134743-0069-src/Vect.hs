8346a24110fdc4e0dd6c6c159475624dfbd0f104
nushio <nushio@nuwheezy.paraiso-lang.org>
2014-07-28 14:19:29 +0900

    paper implemented.

---
+++ LambdaManP/archive/anko-134743-0069-src/Vect.hs
@@ -0,0 +1,43 @@
+module Vect where
+
+
+import DSL
+import Lib
+
+type Pos = (Int,Int)
+
+vecN,vecE,vecS,vecW :: Expr Pos
+vecN = (cons 0 (-1))
+vecE = (cons 1    0)
+vecS = (cons 0    1)
+vecW = (cons (-1) 0)
+      
+type Direction = Int
+       
+vofd :: Expr Direction -> Expr Pos
+vofd d = ite (d.<=1) (ite (d.==0) vecN vecE) $
+         ite (d.==2) vecS vecW
+
+vpeek :: Expr Pos -> Expr (Mat Int) -> Expr Int
+vpeek pos chizu = peekMat (car pos) (cdr pos) chizu
+
+vpoke :: Expr Pos -> Expr Int -> Expr (Mat Int) -> Expr (Mat Int)
+vpoke pos val chizu = pokeMat (car pos) (cdr pos) val chizu
+
+veq :: Expr Pos -> Expr Pos -> Expr Int
+veq  pos vect = (car pos .== car vect) * (cdr pos .== cdr vect)
+
+vadd :: Expr Pos -> Expr Pos -> Expr Pos
+vadd pos vect = cons (car pos + car vect) (cdr pos + cdr vect)
+
+vsub :: Expr Pos -> Expr Pos -> Expr Pos
+vsub pos vect = cons (car pos - car vect) (cdr pos - cdr vect)
+
+vinner :: Expr Pos -> Expr Pos -> Expr Int
+vinner pos vect = (car pos * car vect) + (cdr pos * cdr vect)
+
+vrotR :: Expr Pos -> Expr Pos
+vrotR vect = cons (negate $ cdr vect) (car vect)
+
+vrotL :: Expr Pos -> Expr Pos
+vrotL vect = cons (cdr vect) (negate $ car vect)
