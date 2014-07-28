ca3118bf979f158f56df6197128dff338af224c6
nushio <nushio@nuwheezy.paraiso-lang.org>
2014-07-28 09:14:07 +0900

    mito

--- LambdaManP/mito.hs
+++ LambdaManP/mito.hs
@@ -18,6 +18,7 @@ import Text.Printf
 import Desugar
 import DSL
 import Lib
+import Vect
 import System.Process
 
 ---- vector libraries
@@ -26,50 +27,82 @@ import System.Process
 
 
 -----
-type X = Int
+data X = X
+exprX :: Expr X
+exprX = cast (Const 0)
 type Direction = Int
 type Clock = Int
-type AllState = (AiState,World)
-type World = (Mat Int, (ManState, ([GhostState], FruitState)))
-type World0 = ([[Int]], (ManState, ([GhostState], FruitState)))
+type Environ = (AIState,World)
+type World = ([[Int]], (ManState, ([GhostState], FruitState)))
+
 --               vit                    lives score
 type ManState = (Int, (Pos, (Direction, (Int, Int  ))))
 type GhostState = (Int, (Pos , Direction))
 type FruitState = Int
 type AIState = ((Mat Int),(Clock,X))
-type Pos = (Int, Int)
 
+manStateE :: Expr Environ -> Expr ManState
+manStateE = caddr
+
+chizuE :: Expr Environ -> Expr (Mat Int)
+chizuE = car . car
+
+manPosE :: Expr Environ -> Expr Pos
+manPosE = cadr . manStateE
+
+
+(dirValueTotal :: Expr Environ -> Expr Pos -> Expr Int, dirValueTotalDef) = 
+  def2 "dirValueTotal" $ \env vec -> comp $ do
+    e $ vpeek (manPosE env `vadd` vec) (chizuE env)
 
 (step :: Expr AIState -> Expr World -> Expr (AIState, Int), stepDef) =
-  def2 "step" $ \aist world -> 
+  def2 "step" $ \aist world -> comp $ do
     let 
-        allState = cons 
-        world = cons chizu (cdr world0)
+        env :: Expr Environ
+        env = cons aist world
+        
         clk :: Expr Clock
-        clk = car $ cdr aist
-      
+        clk = cadr aist
+        
+        chizu :: Expr (Mat Int)
+        chizu = car aist
+              
         manState :: Expr ManState
         manState = car $ cdr world        
         manP :: Expr Pos
         manP = car $ cdr $ manState
 
-
       
         newAist :: Expr AIState
-        newAist = cons (mapPoke manP (negate clk) chizu) $
+        newAist = cons (vpoke manP (negate clk) chizu) $
                   cons (clk+1) (cdr $cdr aist)
-        dirToGo = 0
-    in 
-        cons newAist dirToGo
+        
+        scoreN, scoreE, scoreS, scoreW :: Expr Int
+        scoreN = dirValueTotal env vecN 
+        scoreE = dirValueTotal env vecE 
+        scoreS = dirValueTotal env vecS 
+        scoreW = dirValueTotal env vecW 
+
+        dirToGo :: Expr Direction
+        dirToGo 
+           = ite ((scoreN .>= scoreE) + (scoreN .>= scoreS) + (scoreN .>= scoreW) .== 3) 0 $
+             ite ((scoreE .>= scoreS) + (scoreE .>= scoreW) .== 2) 1 $
+             ite (scoreS .>= scoreW) 2 $
+             (3 :: Expr Int)
+        
+    e $ cons newAist dirToGo
       
 progn :: LMan ()
 progn = do
   libDef  
   stepDef  
+  dirValueTotalDef
   
-  let chizu = toMat $ car (Var (-1) 0 :: Expr World0)
-  rtn $ cons (cons chizu (cons 0 0) :: Expr AIState) $ Closure "step"
+  let chizu = toMat $ car (Var (-1) 0 :: Expr World)
+      aist :: Expr AIState
+      aist = cons chizu (cons (Const 0) exprX) 
+  rtn $ cons aist $ Closure "step"
 
 main :: IO ()
 main = do
-  writeFile "../LambdaMan/cornercase-think.gcc" $ compile progn    
+  writeFile "../LambdaMan/mito.gcc" $ compile progn    
