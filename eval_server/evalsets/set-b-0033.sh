a5a4604e717d57dbf16bb194a5892aeb3ce3205a
nushio <nushio@nuwheezy.paraiso-lang.org>
2014-07-28 10:26:36 +0900

    map add

---
+++ eval_server/evalsets/set-b-0033.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+root="$(dirname "$0")/../.."
+
+exec "$root/simulator/simulator" "$root/map/gen/eval-b-0033.map" /dev/stdin "$root/ghost/chase_with_random.ghc,$root/ghost/scatter.ghc,$root/ghost/random_and_chase.ghc,$root/ghost/chase_fixed.ghc"
